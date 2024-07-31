9695fc947a394aaa7322217adfb6db59e7b2d300
suhasledange <suhasledange10@gmail.com>
2024-07-31 13:47:29 +0530

    testing in production

--- middleware.js
+++ middleware.js
@@ -5,7 +5,14 @@ export function middleware(request) {
 
   const isPublicPath = path === '/'
 
-  const token = request.cookies.get('token')?.value || ''
+  const token = request.cookies.get("token")?.value || ''
+
+
+console.log("Middleware")
+
+  console.log("Environment:", process.env.NODE_ENV);
+  console.log("Token Set:", token);
+  console.log("Token Removed:", request.cookies.get("token"));
 
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL('/dashboard', request.nextUrl))
